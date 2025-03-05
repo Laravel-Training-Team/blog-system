0b075bc3d6cb7978bf2a4eefe5af9bcc4a1559ae
omar-920 <omarhessain920@gmail.com>
2025-03-05 22:13:33 +0200

    convert Blog To Blade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('dashboard.pages.home');
+})->name('dashboard');
+
+Route::get('/',function(){
+    return view('blog.pages.posts');
 })->name('index');
+Route::get('/about',function(){
+    return view('blog.pages.about');
+})->name('about.view');
+Route::get('/categories',function(){
+    return view('blog.pages.category');
+})->name('category.view');
+Route::get('/contact',function(){
+    return view('blog.pages.contact');
+})->name('contact.view');
+Route::get('/blog-single',function(){
+    return view('blog.pages.blog-single');
+})->name('blog-single.view');
